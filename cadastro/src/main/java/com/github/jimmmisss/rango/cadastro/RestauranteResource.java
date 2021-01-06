bb44ef82c469848dcc150f9cad4e167f30732249
Wesley Pereira <jimmmisss@gmail.com>
2021-01-06 19:33:13 -0300

    Implementando metodos restantes de controller de retaurantes

--- cadastro/src/main/java/com/github/jimmmisss/rango/cadastro/RestauranteResource.java
+++ cadastro/src/main/java/com/github/jimmmisss/rango/cadastro/RestauranteResource.java
@@ -1,11 +1,15 @@
 package com.github.jimmmisss.rango.cadastro;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Optional;
+
+import static javax.ws.rs.core.Response.Status.CREATED;
 
 @Path("/restaurantes")
 @Produces(MediaType.APPLICATION_JSON)
@@ -15,5 +19,35 @@ public class RestauranteResource {
     @GET
     public List<Restaurante> listaRestaurantes() {
         return Restaurante.listAll();
+    }
+
+    @POST
+    @Transactional
+    public Response salvar(Restaurante dto) {
+        dto.persist();
+        return Response.status(CREATED).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public void update(@PathParam("id") Long id, Restaurante dto) {
+        Optional<Restaurante> restauranteResult = Restaurante.findByIdOptional(id);
+        if (restauranteResult.isEmpty()) {
+            throw new NotFoundException();
+        }
+        Restaurante restaurante = restauranteResult.get();
+        restaurante.nome = dto.nome;
+        restaurante.persist();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public void update(@PathParam("id") Long id) {
+        Optional<Restaurante> restauranteResult = Restaurante.findByIdOptional(id);
+        restauranteResult.ifPresentOrElse(Restaurante::delete, () -> {
+            throw new NotFoundException();
+        });
     }
 }
